7195c10a1a6365fcc4d6415e56e8fc26b61c4fbe
Gary <garyivanov89@gmail.com>
2018-02-14 21:36:03 -0500

    added form

---
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+        //static private List<string> cheeses = new List<string>();
+        static private Dictionary<string, string> cheeses = new Dictionary<string, string>();
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            ViewBag.cheeses = cheeses;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, string description)
+        {
+            cheeses.Add(name,description);
+
+            return Redirect("/Cheese");
+        }
+    }
+}
